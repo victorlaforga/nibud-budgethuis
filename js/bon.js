f2dce07d701fdce847113ea0c72aa65ea7130764
Victor <victorlaforga@hotmail.com>
2020-01-21 18:27:53 +0100

    final edit house

--- js/bon.js
+++ js/bon.js
@@ -1,36 +1,43 @@
-const bon1 = +document.getElementById("myRange").value;
-const bon2 = +document.getElementById("myRange2").value;
-const bon3 = +document.getElementById("myRange3").value;
-const bon4 = +document.getElementById("myRange4").value;
-const bon5 = +document.getElementById("myRange5").value;
-const bon6 = +document.getElementById("myRange6").value;
-const bon7 = +document.getElementById("myRange7").value;
-const bon8 = +document.getElementById("myRange8").value;
-const bon9 = +document.getElementById("myRange9").value;
 
 
 
-function sumUp(){
-    let totaal = bon1 + bon2 + bon3 + bon4 + bon5 + bon6 + bon7 + bon8 + bon9;
-    console.log(totaal);
+document.querySelector('#myRange').onchange = calculateAll;
+document.querySelector('#myRange2').onchange = calculateAll;
+document.querySelector('#myRange3').onchange = calculateAll;
+document.querySelector('#myRange4').onchange = calculateAll;
+document.querySelector('#myRange5').onchange = calculateAll;
+document.querySelector('#myRange6').onchange = calculateAll;
+document.querySelector('#myRange7').onchange = calculateAll;
+document.querySelector('#myRange8').onchange = calculateAll;
+document.querySelector('#myRange9').onchange = calculateAll;
+document.querySelector("#maandelijksinkomen").onchange = calculateAll;
+document.querySelector("#maandelijksekosten").onchange = calculateAll;
+
+
+function calculateAll(){
+    const inkomen = +document.querySelector("#maandelijksinkomen").value;
+    const huur = +document.querySelector("#maandelijksekosten").value;
+    const x = +document.querySelector('#myRange').value;
+    const x2 = +document.querySelector('#myRange2').value;
+    const x3 = +document.querySelector('#myRange3').value;
+    const x4 = +document.querySelector('#myRange4').value;
+    const x5 = +document.querySelector('#myRange5').value;
+    const x6 = +document.querySelector('#myRange6').value;
+    const x7 = +document.querySelector('#myRange7').value;
+    const x8 = +document.querySelector('#myRange8').value;
+    const x9 = +document.querySelector('#myRange9').value;
     
+    let totaal = inkomen - huur - x - x2 - x3 - x4 - x5 - x6 -x7 -x8 -x9;
     document.getElementById("totaalBon").innerHTML = totaal;
-    function colorTotal(){
-        const totaalBon = document.getElementById("totaalBon");
-        // const totaalTitel = document.getElementsById("totalTitel");
+
         if(totaal > 0) {
-            totaalBon.style.color = "green";
-            // totaalTitel.style.color = "green";
-        }
-        else {
-            totaalBon.style.color = "red";
-            // totaalTitel.style.color = "red";
-        }
-    }
-    colorTotal();
-}
-sumUp();
-const inkomenBon = document.getElementById("inkomenBon");
-function turnFontBold() {
+                    totaalBon.style.color = "green";
+                    // totaalTitel.style.color = "green";
+                }
+                else {
+                    totaalBon.style.color = "red";
+                    // totaalTitel.style.color = "red";
+                }
     
-    }
+}
+
